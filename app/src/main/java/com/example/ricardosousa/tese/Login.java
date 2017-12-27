aee95da676e5502220af2a146642e47b63bf816c
SardaoDaNoite <34687330+SardaoDaNoite@users.noreply.github.com>
2017-12-27 18:04:56 +0000

    hghjg

--- app/src/main/java/com/example/ricardosousa/tese/Login.java
+++ app/src/main/java/com/example/ricardosousa/tese/Login.java
@@ -1,10 +1,15 @@
 package com.example.ricardosousa.tese;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewParent;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
@@ -22,5 +27,10 @@ public class Login extends AppCompatActivity {
                 startActivity(AMenu);
             }
         });
+
+        TextView simbol = (TextView) findViewById(R.id.textView);
+        Typeface font = Typeface.createFromAsset(getAssets(),"Fonts/fontawesome-webfont.ttf");
+        simbol.setTypeface(font);
+        simbol.setText("\uf023");
     }
 }

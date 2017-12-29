2b18855ebb94ebc9765742de6f72de18e1a289fa
SardaoDaNoite <34687330+SardaoDaNoite@users.noreply.github.com>
2017-12-29 17:43:11 +0000

    hghjg

--- app/src/main/java/com/example/ricardosousa/tese/Menu.java
+++ app/src/main/java/com/example/ricardosousa/tese/Menu.java
@@ -1,8 +1,13 @@
 package com.example.ricardosousa.tese;
 
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,12 +18,12 @@ import java.util.Calendar;
 import java.util.Currency;
 import java.util.Date;
 
-public class Menu extends AppCompatActivity {
+public class Menu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private Handler handler;
     private TextView Data = null;
     private TextView Hora = null;
     SimpleDateFormat Hora_Format = new SimpleDateFormat("HH:mm");
-    SimpleDateFormat Data_Format = new SimpleDateFormat("E ,d  MMM");
+    SimpleDateFormat Data_Format = new SimpleDateFormat("E  d  MMM");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +43,25 @@ public class Menu extends AppCompatActivity {
             Data.setText(data);
             Hora.setText(hora);
             handler.postDelayed(this,1000);
+            NavigationView esq = (NavigationView)findViewById(R.id.nvView);
         }
     };
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.Pacientes){
+
+        }
+        else if (id == R.id.Horario){
+
+        }
+        else if (id == R.id.Mensagens){
+
+        }
+        else if (id == R.id.Configuracoes){
+
+        }
+        return false;
+    }
 }

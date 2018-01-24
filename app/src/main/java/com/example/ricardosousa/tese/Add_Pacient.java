e6842f868fcad3f7150c5ac143085293da282b64
SardaoDaNoite <34687330+SardaoDaNoite@users.noreply.github.com>
2018-01-24 15:30:41 +0000

    hghjg

--- app/src/main/java/com/example/ricardosousa/tese/Add_Pacient.java
+++ app/src/main/java/com/example/ricardosousa/tese/Add_Pacient.java
@@ -2,6 +2,8 @@ package com.example.ricardosousa.tese;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Add_Pacient extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Add_Pacient extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add__pacient);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.gender, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }

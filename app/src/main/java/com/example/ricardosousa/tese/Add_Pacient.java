21b62db02ea896821423a105476ff9b6f0e6a053
SardaoDaNoite <34687330+SardaoDaNoite@users.noreply.github.com>
2018-01-25 22:11:04 +0000

    fef

--- app/src/main/java/com/example/ricardosousa/tese/Add_Pacient.java
+++ app/src/main/java/com/example/ricardosousa/tese/Add_Pacient.java
@@ -1,12 +1,14 @@
 package com.example.ricardosousa.tese;
 
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
@@ -48,23 +50,11 @@ public class Add_Pacient extends AppCompatActivity {
                         v.setVisibility(View.GONE);
                     }
                 }
-                EditText Descriçao = new EditText(getApplicationContext());
-                LinearLayout.LayoutParams params= new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-                float d = getApplicationContext().getResources().getDisplayMetrics().density;
-                params.setMargins((int)(10*d),(int)(10*d),(int)(10*d),(int)(10*d));
-                Descriçao.setBackground(getDrawable(R.drawable.edittext));
-                Descriçao.setElevation(10*d);
-                Descriçao.setEms(10);
-                Descriçao.setPadding((int)(8*d),(int)(8*d),(int)(8*d),(int)(8*d));
-                Descriçao.setTextColor(getColor(R.color.branco));
-                Descriçao.setHintTextColor(getColor(R.color.branco));
-                Descriçao.setSingleLine(false);
-                Descriçao.setImeOptions(EditorInfo.IME_FLAG_NO_ENTER_ACTION);
-
+                LayoutInflater layoutInflater =
+                        (LayoutInflater) getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 for (int i = 0; i < numero; i++) {
-                    Descriçao.setId(9000+i);
-                    Descriçao.setHint("Descriçao");
-                    feridas.addView(Descriçao,params);
+                    View addView = layoutInflater.inflate(R.layout.feridas, null);
+                    feridas.addView(addView);
                 }
             }
         });
